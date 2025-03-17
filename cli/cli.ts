eebe20f4c530c814da67f2a74799fa5fcb54fd2d
Jacqueline Amherst <jacqueline.amherst@icloud.com>
2025-03-17 17:20:39 -0500

    fill in cli function calls

--- cli/cli.ts
+++ cli/cli.ts
@@ -1,4 +1,9 @@
 import { Command } from "commander";
+import { setupRabbitoryRoleWithPolicy } from "../aws/IAM/createRabbitoryRole";
+import { setupBrokerRoleWithPolicy } from "../aws/IAM/createBrokerRole";
+import { createDashboard } from "../aws/EC2/createDashboard";
+import { createTable } from "../aws/dynamoDB/createTable";
+import { create } from "domain";
 
 const program = new Command();
 
@@ -14,8 +19,12 @@ program
     // Confirm that AWS CLI is installed
     // Confirm that user account is linked to AWS
     // Run IAM script
+    await setupRabbitoryRoleWithPolicy();
+    await setupRabbitoryRoleWithPolicy();
     // Run EC2 script
+    await createDashboard();
     // Run database script
+    await createTable();
   });
 
 program.parse();
