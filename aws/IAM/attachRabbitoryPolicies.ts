1eb009e99196f0d7ca8094e8a8f045af480d4885
larencozart <larencozart@gmail.com>
2025-03-17 10:09:00 -0500

    Change file/function name to attachRabbitoryPolicies

--- aws/IAM/attachRabbitoryPolicies.ts
+++ aws/IAM/attachRabbitoryPolicies.ts
@@ -24,7 +24,7 @@ const attachPolicy = async (policyArn: string) => {
   }
 };
 
-export const attachPolicies = async () => {
+export const attachRabbitoryPolicies = async () => {
   try {
     for (const policy of policies) {
       await attachPolicy(policy);
