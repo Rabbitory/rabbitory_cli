4640c3e4d0166ab6b9bad440bce55926971bf044
larencozart <larencozart@gmail.com>
2025-03-17 11:25:50 -0500

    Refactor createRabbitoryRole file to combine policy attachment

--- aws/IAM/createRabbitoryRole.ts
+++ aws/IAM/createRabbitoryRole.ts
@@ -1,10 +1,11 @@
-import { CreateRoleCommand, IAMClient, CreateRoleResponse } from "@aws-sdk/client-iam";
+import { CreateRoleCommand, AttachRolePolicyCommand, IAMClient, CreateRoleResponse } from "@aws-sdk/client-iam";
 
 const REGION = 'us-east-1';
 const client = new IAMClient({ region: REGION });
 
 export const ROLE_NAME = "RabbitoryRole";
 
+// Role creation request
 const ROLE_REQUEST = {
   AssumeRolePolicyDocument: JSON.stringify({
     Version: "2012-10-17",
@@ -21,7 +22,7 @@ const ROLE_REQUEST = {
   RoleName: ROLE_NAME,
 };
 
-export const createRabbitoryRole = async (): Promise<string | null> => {
+const createRabbitoryRole = async (): Promise<string | null> => {
   try {
     const command = new CreateRoleCommand(ROLE_REQUEST);
     const response: CreateRoleResponse = await client.send(command);
@@ -36,5 +37,48 @@ export const createRabbitoryRole = async (): Promise<string | null> => {
   } catch (error) {
     console.error("Error creating role:", error);
     return null; // Return null in case of error
+  }
+};
+
+const policies = [
+  // "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+  "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
+  "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
+];
+
+const attachPolicy = async (policyArn: string) => {
+  try {
+    const command = new AttachRolePolicyCommand({
+      RoleName: ROLE_NAME,
+      PolicyArn: policyArn,
+    });
+    await client.send(command);
+    console.log(`Policy ${policyArn} attached successfully`);
+  } catch (error) {
+    console.error(`Error attaching policy ${policyArn}:`, error);
+  }
+};
+
+const attachRabbitoryPolicies = async () => {
+  try {
+    for (const policy of policies) {
+      await attachPolicy(policy);
+    }
+  } catch (error) {
+    console.error("Error attaching policies:", error);
+  }
+};
+
+// Function that creates the role and attaches the policies in sequence
+export const createAndAttachRabbitoryRole = async () => {
+  try {
+    const roleArn = await createRabbitoryRole();
+    if (roleArn) {
+      await attachRabbitoryPolicies();
+    } else {
+      console.error("Role creation failed, policies not attached.");
+    }
+  } catch (error) {
+    console.error("Error in creating role and attaching policies:", error);
   }
 };
