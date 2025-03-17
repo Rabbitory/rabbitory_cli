5e184503516408ff7a3a8b89f7a35256a4495212
larencozart <larencozart@gmail.com>
2025-03-17 09:49:53 -0500

    Add exports and return of ARN from createRole function

--- aws/IAM/createRabbitoryRole.ts
+++ aws/IAM/createRabbitoryRole.ts
@@ -1,7 +1,10 @@
-import { CreateRoleCommand, IAMClient } from "@aws-sdk/client-iam";
+import { CreateRoleCommand, IAMClient, CreateRoleResponse } from "@aws-sdk/client-iam";
 
 const REGION = 'us-east-1';
-const ROLE_NAME = "RabbitoryRole";
+const client = new IAMClient({ region: REGION });
+
+export const ROLE_NAME = "RabbitoryRole";
+
 const ROLE_REQUEST = {
   AssumeRolePolicyDocument: JSON.stringify({
     Version: "2012-10-17",
@@ -9,7 +12,7 @@ const ROLE_REQUEST = {
       {
         Effect: "Allow",
         Principal: {
-          Service: "ec2.amazonaws.com", // we are placing backend in EC2 to start!
+          Service: "ec2.amazonaws.com",
         },
         Action: "sts:AssumeRole",
       },
@@ -18,47 +21,20 @@ const ROLE_REQUEST = {
   RoleName: ROLE_NAME,
 };
 
-const client = new IAMClient({ region: REGION }); 
-
-export const createRole = async (roleName: string) => {
+export const createRole = async (): Promise<string | null> => {
   try {
     const command = new CreateRoleCommand(ROLE_REQUEST);
-    const response = await client.send(command);
-    console.log("Role created successfully:", response);
-    return response;
+    const response: CreateRoleResponse = await client.send(command);
+
+    if (response.Role && response.Role.Arn) {
+      console.log("Role created successfully:", response.Role.Arn);
+      return response.Role.Arn; // Return the ARN
+    } else {
+      console.error("Error: Role or Role ARN is undefined");
+      return null;
+    }
   } catch (error) {
     console.error("Error creating role:", error);
+    return null; // Return null in case of error
   }
-  
-}
-
-// createRole(ROLE_NAME);
-
-// Response from client.send() will look like this:
-
-// { // CreateRoleResponse
-//   Role: { // Role
-//     Path: "STRING_VALUE", // required
-//     RoleName: "STRING_VALUE", // required
-//     RoleId: "STRING_VALUE", // required
-//     Arn: "STRING_VALUE", // required
-//     CreateDate: new Date("TIMESTAMP"), // required
-//     AssumeRolePolicyDocument: "STRING_VALUE",
-//     Description: "STRING_VALUE",
-//     MaxSessionDuration: Number("int"),
-//     PermissionsBoundary: { // AttachedPermissionsBoundary
-//       PermissionsBoundaryType: "PermissionsBoundaryPolicy",
-//       PermissionsBoundaryArn: "STRING_VALUE",
-//     },
-//     Tags: [ // tagListType
-//       { // Tag
-//         Key: "STRING_VALUE", // required
-//         Value: "STRING_VALUE", // required
-//       },
-//     ],
-//     RoleLastUsed: { // RoleLastUsed
-//       LastUsedDate: new Date("TIMESTAMP"),
-//       Region: "STRING_VALUE",
-//     },
-//   },
-// };CreateRoleCommand, IAMClient, 
+};
