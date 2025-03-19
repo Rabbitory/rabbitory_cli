ece597a0eb9f814abaca622607815a765594da38
larencozart <larencozart@gmail.com>
2025-03-19 10:08:34 -0500

    Cleanup logging and error handling

--- aws/EC2/deleteInstance.ts
+++ aws/EC2/deleteInstance.ts
@@ -1,26 +1,25 @@
 import { EC2Client, TerminateInstancesCommand, waitUntilInstanceTerminated } from "@aws-sdk/client-ec2";
 
 const REGION = "us-east-1";
-
 const ec2Client = new EC2Client({ region: REGION });
 
 export const deleteInstance = async (id: string) => {
   try {
-    const data = await ec2Client.send(
+    await ec2Client.send(
       new TerminateInstancesCommand({
         InstanceIds: [id],
       }),
     );
 
-    console.log("Instance deletion started:", id);
-
     await waitUntilInstanceTerminated(
       { client: ec2Client, maxWaitTime: 240 },
       { InstanceIds: [id] }
     );
-    console.log(`Instance ${id} terminated successfully.`);
-    return data;
   } catch (err) {
-    console.error("Error deleting instance:", err);
+    if (err instanceof Error) {
+      throw new Error(`Error deleting instance ${id}\n${err.message}`);
+    } else {
+      throw new Error(`Unknown error deleting instance ${id}\n${String(err)}`);
+    }
   }
 };
