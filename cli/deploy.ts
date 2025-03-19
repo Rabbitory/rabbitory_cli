cf63473c7a88eb6120164514b833452f4c807bed
larencozart <larencozart@gmail.com>
2025-03-19 10:39:54 -0500

    Add spinner for aws api calls is destroy file

--- cli/deploy.ts
+++ cli/deploy.ts
@@ -4,14 +4,14 @@ import { createRMQBrokerIAM } from "../aws/IAM/createBrokerRole";
 import { setupRabbitorySG } from "../aws/security-groups/createRabbitoryEngineSG";
 import { createDashboard } from "../aws/EC2/createDashboard";
 import { createTable } from "../aws/dynamoDB/createTable";
-import { runWithSpinner } from "./spinner"; // Importing the spinner utility
+import { runWithSpinner } from "./spinner";
 import chalk from "chalk";
 
 export const deploy = async () => {
   try {
     await runWithSpinner('Setting up Rabbitory Engine IAM...', createRabbitoryEngineIAM, 'Created Rabbitory Engine IAM role and instance profile');
     await runWithSpinner('Setting up RMQ Broker IAM...', createRMQBrokerIAM, 'Created RMQBroker IAM role and instance profile');
-    // await runWithSpinner('Waiting for IAM instance profile to propagate...', () => new Promise((resolve) => setTimeout(resolve, 5000)), 'IAM instance profile propagated');
+    await runWithSpinner('Waiting for IAM instance profile to propagate...', () => new Promise((resolve) => setTimeout(resolve, 5000)), 'IAM instance profile propagated');
     const rabbitorySecurityGroupId = await runWithSpinner('Setting up Rabbitory Engine Security Group...', setupRabbitorySG, 'Created Rabbitory Engine security group');
     await runWithSpinner('Creating Rabbitory Engine EC2 instance...', () => createDashboard(rabbitorySecurityGroupId), 'Created Rabbitory Engine EC2 instance');
     await runWithSpinner('Creating DynamoDB Table..', createTable, 'Created DynamoDB Table');
--- cli/destroy.ts
+++ cli/destroy.ts
@@ -4,21 +4,27 @@ import { deleteBrokerRole } from "../aws/IAM/deleteBrokerRole";
 import { deleteRabbitoryRole } from "../aws/IAM/deleteRabbitoryRole";
 import { deleteInstance } from "../aws/EC2/deleteInstance";
 import { getInstanceByName } from "./getInstanceByName";
+import { runWithSpinner } from "./spinner";
+import chalk from "chalk";
 
 export const destroy = async () => {
-  // const tableName = "RabbitoryTable";
-  const instanceName = "RabbitoryDashboard";
-  const instanceIds: string[] | undefined = await getInstanceByName(instanceName);
+  try {
+    const instanceName = "RabbitoryDashboard";
+    const instanceIds: string[] | undefined = await getInstanceByName(instanceName);
 
-  await deleteTable();
+    await runWithSpinner('Deleting DynamoDB Table...', deleteTable, 'Deleted DynamoDB Table');
 
-  if (instanceIds !== undefined && instanceIds.length > 0) {
-    await deleteInstance(instanceIds[0]);
-  } else {
-    console.log(`No EC2 instance with name "${instanceName} exists`);
+    if (instanceIds !== undefined && instanceIds.length > 0) {
+      await runWithSpinner('Terminating EC2 instance...', () => deleteInstance(instanceIds[0]), 'Terminated EC2 instance');
+    } else {
+      console.log(`No EC2 instance with name "${instanceName}" exists`);
+    }
+
+    await runWithSpinner('Deleting Rabbitory Engine Security Group...', deleteRabbitoryEngineSG, 'Deleted Rabbitory Engine Security Group');
+    await runWithSpinner('Deleting RMQ Broker IAM role...', deleteBrokerRole, 'Deleted RMQ Broker IAM role');
+    await runWithSpinner('Deleting Rabbitory Engine IAM role...', deleteRabbitoryRole, 'Deleted Rabbitory Engine IAM role');
+
+  } catch (error) {
+    console.error(chalk.redBright("\nRabbitory destruction failed\n"), error, "\n");
   }
-
-  await deleteRabbitoryEngineSG();
-  await deleteBrokerRole();
-  await deleteRabbitoryRole();
-}
+};
