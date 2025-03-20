3c369b46cc241d5e1ff50a576d9e5b936b4f1b7f
Jacqueline Amherst <jacqueline.amherst@icloud.com>
2025-03-20 16:43:56 -0500

    add getRegion file

--- cli/deploy.ts
+++ cli/deploy.ts
@@ -4,19 +4,21 @@ import { setupRabbitorySG } from "../aws/security-groups/createRabbitoryEngineSG
 import { createDashboard } from "../aws/EC2/createDashboard";
 import { createTable } from "../aws/dynamoDB/createTable";
 import { runWithSpinner } from "./spinner";
+import { getRegion } from "./getRegion";
 import { logo } from "./logo";
 import chalk from "chalk";
 
 export const deploy = async () => {
   try {
-    await runWithSpinner('Setting up Rabbitory Engine IAM...', createRabbitoryEngineIAM, 'Created Rabbitory Engine IAM role and instance profile');
-    await runWithSpinner('Setting up RMQ Broker IAM...', createRMQBrokerIAM, 'Created RMQBroker IAM role and instance profile');
-    await runWithSpinner('Waiting for IAM instance profile to propagate...', () => new Promise((resolve) => setTimeout(resolve, 5000)), 'IAM instance profile propagated');
-    const rabbitorySecurityGroupId = await runWithSpinner('Setting up Rabbitory Engine Security Group...', setupRabbitorySG, 'Created Rabbitory Engine security group');
-    await runWithSpinner('Creating Rabbitory Engine EC2 instance...', () => createDashboard(rabbitorySecurityGroupId), 'Created Rabbitory Engine EC2 instance');
-    await runWithSpinner('Creating DynamoDB Table..', createTable, 'Created DynamoDB Table');
-
-    console.log(logo);
+    await getRegion();
+    // await runWithSpinner('Setting up Rabbitory Engine IAM...', createRabbitoryEngineIAM, 'Created Rabbitory Engine IAM role and instance profile');
+    // await runWithSpinner('Setting up RMQ Broker IAM...', createRMQBrokerIAM, 'Created RMQBroker IAM role and instance profile');
+    // await runWithSpinner('Waiting for IAM instance profile to propagate...', () => new Promise((resolve) => setTimeout(resolve, 5000)), 'IAM instance profile propagated');
+    // const rabbitorySecurityGroupId = await runWithSpinner('Setting up Rabbitory Engine Security Group...', setupRabbitorySG, 'Created Rabbitory Engine security group');
+    // await runWithSpinner('Creating Rabbitory Engine EC2 instance...', () => createDashboard(rabbitorySecurityGroupId), 'Created Rabbitory Engine EC2 instance');
+    // await runWithSpinner('Creating DynamoDB Table..', createTable, 'Created DynamoDB Table');
+    //
+    console.log(chalk.red(logo));
   } catch (error) {
     console.error(chalk.redBright("\nRabbitory deployment failed\n"), error, "\n");
   }
