2126d18ada533f2216f7ed828a902f265b31401f
larencozart <larencozart@gmail.com>
2025-03-26 20:56:44 -0500

    Update jest.config.ts and tsconfig.json for testing

--- jest.config.ts
+++ jest.config.ts
@@ -3,14 +3,14 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
-  roots: ["<rootDir>/src"], // Ensures Jest looks in "src" instead of "dist"
-  testMatch: ["**/_tests_/**/*.test.ts"], // Matches test files inside _tests_ folders
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"], // Ensures TS files are recognized
+  roots: ["<rootDir>/aws", "<rootDir>/cli"],
+  testMatch: ["**/_tests_/**/*.test.ts"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transform: {
-    "^.+\\.tsx?$": "ts-jest", // Transforms TypeScript files with ts-jest
+    "^.+\\.tsx?$": "ts-jest",
   },
-  collectCoverage: true, // Optional: Collects test coverage
-  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"], // Ignores node_modules & dist
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
 
 export default config;
