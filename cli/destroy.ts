44b1fa1ca9e8b1aa66883397d07756c3702bb728
jqln <jacqueline.amherst@icloud.com>
2025-04-01 18:31:36 -0500

    delete SG, brokers, IAM, etc in every region

--- cli/destroy.ts
+++ cli/destroy.ts
@@ -8,57 +8,69 @@ import { getInstanceIdsByPublisher } from "./getInstanceIdsByPublisher";
 import { runWithSpinner } from "./spinner";
 import { getRegion } from "./getRegion";
 import chalk from "chalk";
+import { fetchAllRegions } from "./fetchAllRegions";
 
 export const destroy = async () => {
   try {
     const controlPanelName = "RabbitoryControlPanel";
-    const region: string = await getRegion();
-    const brokerIds = await getInstanceIdsByPublisher("Rabbitory", region);
+    const primaryRegion: string = await getRegion();
+
+    const regions = await fetchAllRegions();
+
+    if (!regions || regions.length === 0) {
+      console.error("Error fetching regions");
+      process.exit(1);
+    }
+
+    await runWithSpinner(
+      "Deleting DynamoDB Table...",
+      () => deleteTable(primaryRegion),
+      "Deleted DynamoDB Table",
+    );
+
     const instanceIds: string[] | undefined = await getRunningInstanceIdsByName(
       controlPanelName,
-      region,
+      primaryRegion,
     );
     const instanceId: string | undefined =
       instanceIds !== undefined && instanceIds.length > 0
         ? instanceIds[0]
         : undefined;
 
-    await runWithSpinner(
-      "Deleting DynamoDB Table...",
-      () => deleteTable(region),
-      "Deleted DynamoDB Table",
-    );
-
-    await runWithSpinner(
-      "Deleting RabbitMQ Broker Instances...",
-      () =>
-        Promise.all(brokerIds?.map((id) => deleteInstance(id, region)) || []),
-      "Deleted RabbitMQ Broker Instances",
-    );
-
     if (instanceId !== undefined) {
       await runWithSpinner(
         "Terminating Control Panel EC2 instance...",
-        () => deleteInstance(instanceId, region),
+        () => deleteInstance(instanceId, primaryRegion),
         "Terminated EC2 instance",
       );
     }
 
-    await runWithSpinner(
-      "Deleting Rabbitory security group...",
-      () => deleteRabbitorySG(region),
-      "Deleted Rabbitory security group",
-    );
-    await runWithSpinner(
-      "Deleting RMQ Broker IAM role...",
-      () => deleteBrokerRole(region),
-      "Deleted RMQ Broker IAM role",
-    );
-    await runWithSpinner(
-      "Deleting Rabbitory IAM role...",
-      () => deleteRabbitoryRole(region),
-      "Deleted Rabbitory IAM role",
-    );
+    for (const region of regions) {
+      const brokerIds = await getInstanceIdsByPublisher("Rabbitory", region);
+
+      await runWithSpinner(
+        "Deleting RabbitMQ Broker Instances...",
+        () => Promise.all(brokerIds?.map((id) => deleteInstance(id, region)) || []),
+        "Deleted RabbitMQ Broker Instances",
+      );
+
+      await runWithSpinner(
+        "Deleting Rabbitory security group...",
+        () => deleteRabbitorySG(region),
+        "Deleted Rabbitory security group",
+      );
+
+      await runWithSpinner(
+        "Deleting RMQ Broker IAM role...",
+        () => deleteBrokerRole(region),
+        "Deleted RMQ Broker IAM role",
+      );
+      await runWithSpinner(
+        "Deleting Rabbitory IAM role...",
+        () => deleteRabbitoryRole(region),
+        "Deleted Rabbitory IAM role",
+      );
+    }
   } catch (error) {
     console.error(
       chalk.redBright("\nRabbitory destruction failed\n"),
